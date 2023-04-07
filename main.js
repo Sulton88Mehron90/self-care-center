81cc85a6b09f872853cd2a0a2811d39d03e28049
Parvin Sattorova <nanajon66@gmail.com>
2023-04-06 21:15:22 -0600

    feat: add querySelectors

---
+++ main.js
@@ -0,0 +1,8 @@
+//querySelectors
+var receiveMessageBtn = document.querySelector('#btn-receive-msg');
+
+var createAffBtn = document.querySelector('#btn-create-aff');
+var createManBtn = document.querySelector('#btn-create-man');
+
+var affirmationRb = document.querySelector('#rb-affirmation');
+var mantraRb = document.querySelector('#rb-mantra');
