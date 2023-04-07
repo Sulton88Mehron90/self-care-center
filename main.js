a9802171b11d3e26ef7ff0b991ce8cd700158c5b
Parvin Sattorova <nanajon66@gmail.com>
2023-04-06 21:59:07 -0600

    feat: add afformation montra js file.

--- main.js
+++ main.js
@@ -3,6 +3,6 @@ var receiveMessageBtn = document.querySelector('#btn-receive-msg');
 
 var createAffBtn = document.querySelector('#btn-create-aff');
 var createManBtn = document.querySelector('#btn-create-man');
-
 var affirmationRb = document.querySelector('#rb-affirmation');
 var mantraRb = document.querySelector('#rb-mantra');
+
